02ad99f3d8603980c78e8082e2fc19a2531a2bf0
Qasim Hussain <qasim.hussain1@kpmg.co.uk>
2023-01-19 15:45:35 +0000

    Test

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+const PlaceholderImage = 'https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg/preview'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: '#fff' } }>Shake-N-Make Project Spiking</Text>
+      <View> style={styles.imageContainer}
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a cocktail" />
+        <Button label="Random cocktail" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  // imageContainer: {
+  //   flex: 1,
+  //   paddingTop: 58,
+  // },
+  // image: {
+  //   width: 320,
+  //   height: 440,
+  //   borderRadius: 18,
+  // },
 });
