2fea2c07f7dcddb01aa471b16ba0911324e171f6
Bengmoody <bengmoody@hotmail.co.uk>
2023-01-20 10:36:32 +0000

    working firebase branch for grabbing remote data and writing remote data manually

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import firestore from "@react-native-firebase/firestore";
+
+
+const bookGetter = () => {
+  // return firestore().collection('books').doc('9Idt8fZgE4zhodn2y4fm').get()
+  return firestore().collection('books').get()
+  .then((res) => {
+    res.forEach((element) => {
+      console.log({...element.data()})
+    })
+  })
+}
+const bookMaker = () => {
+  const docRef = firestore().collection("users").doc().set({first: "bob",last: "johnson", born: 1815})
+}
+
+let bookDocument=bookGetter()
+// bookMaker()
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
